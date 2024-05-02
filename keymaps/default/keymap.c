602cb7b0b413fd08ea56fb33f66dfb7aeecbb76b
Sigurd Holsen <sigurd.holsen@cognite.com>
2024-05-02 19:06:28 +0200

    More X's

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -62,9 +62,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [NUMB] = LAYOUT( // Hold down right side
         _______, _______, _______, _______,      _______,  _______,      _______,  _______,  _______,  _______,  _______, _______,
 
-        _______, _______,    KC_7,    KC_8,         KC_9,  _______,      _______,    KC_F7,    KC_F8,    KC_F9,   KC_F10,  KC_DEL,
-        _______,    KC_0,    KC_4,    KC_5,         KC_6,     KC_0,      _______,    KC_F4,    KC_F5,    KC_F6,   KC_F11, KC_CAPS,
-        _______, _______,    KC_1,    KC_2,         KC_3,  _______,      _______,    KC_F1,    KC_F2,    KC_F3,   KC_F12, _______,
+        _______, XXXXXXX,    KC_7,    KC_8,         KC_9,  XXXXXXX,      XXXXXXX,    KC_F7,    KC_F8,    KC_F9,   KC_F10,  KC_DEL,
+        _______,    KC_0,    KC_4,    KC_5,         KC_6,     KC_0,      XXXXXXX,    KC_F4,    KC_F5,    KC_F6,   KC_F11, KC_CAPS,
+        _______, XXXXXXX,    KC_1,    KC_2,         KC_3,  XXXXXXX,      XXXXXXX,    KC_F1,    KC_F2,    KC_F3,   KC_F12, XXXXXXX,
                           _______, _______, MO(SETTINGS),  _______,      _______,  _______,  _______,  _______
     ),
 
