7c813453b20b93b49f1a647cb76696cd05c7dc4c
Sigurd Holsen <sighol@gmail.com>
2025-10-31 15:40:07 +0100

    @ and $

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -111,8 +111,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [QUICK_NUMBERS] = LAYOUT(
         _______, _______, _______, _______, _______,  _______,      _______, _______, _______, _______, _______, _______,
-        _______, XXXXXXX,    KC_7,    KC_8,     KC_9,  XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        _______,    KC_0,    KC_4,    KC_5,     KC_6,     KC_0,     XXXXXXX, XXXXXXX, MO(QUICK_SYMBOLS), XXXXXXX, XXXXXXX, XXXXXXX,
+        _______,RALT(KC_4),  KC_7,    KC_8,     KC_9,  XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        _______,RALT(KC_2),  KC_4,    KC_5,     KC_6,     KC_0,     XXXXXXX, XXXXXXX, MO(QUICK_SYMBOLS), XXXXXXX, XXXXXXX, XXXXXXX,
         _______, XXXXXXX,    KC_1,    KC_2,     KC_3,   KC_DOT,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
                            _______, _______, _______,  _______,     _______, _______, _______, _______
     ),
